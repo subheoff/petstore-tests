8e60e2d54b2dc1ffee0dbfcca665c24f5130d4f0
subheoff <74992973+subheoff@users.noreply.github.com>
2025-03-13 19:57:19 +0300

    Update test_store.py

--- tests/test_store.py
+++ tests/test_store.py
@@ -2,18 +2,14 @@ import pytest
 import requests
 
 
-# тест на получение наличия товара
 def test_store_inventory(base_url):
-    # запрос на получение наличия товара
     response = requests.get(f'{base_url}/store/inventory')
     assert response.status_code == 200, f"код {response.status_code}, {response.text}"
 
-    # проверка на наличие ключа available
     response_data = response.json()
     assert "available" in response_data, f"available не найден в {response_data}"
 
 
-# тест на получение заказа по id
 @pytest.mark.parametrize(
     "order_id",
     [
@@ -23,7 +19,6 @@ def test_store_inventory(base_url):
     ]
 )
 def test_store_get_order(base_url, order_id):
-    # запрос на получение
     response = requests.get(f'{base_url}/store/order/{order_id}')
     assert response.status_code == 200, f"код {response.status_code}, {response.text}"
 
@@ -31,9 +26,7 @@ def test_store_get_order(base_url, order_id):
     assert response_data["id"] == order_id, f"id заказа не совпадает"
 
 
-# тест на создание заказа
 def test_store_create_order(base_url):
-    # данные и запрос на создание заказа
     order_data = {
         "id": 10,
         "petId": 10,
@@ -55,7 +48,6 @@ def test_store_create_order(base_url):
     assert response_data["complete"] == order_data["complete"], 'shipDate заказа не совпадают'
 
 
-# тест удаление заказа
 @pytest.mark.parametrize(
     "order_id",
     [
@@ -65,7 +57,6 @@ def test_store_create_order(base_url):
     ]
 )
 def test_delete_order(base_url, order_id):
-    # создаем заказ
     order_data = {
         "id": order_id,
         "petId": 10,
@@ -78,6 +69,6 @@ def test_delete_order(base_url, order_id):
     response = requests.post(f"{base_url}/store/order", json=order_data)
     assert response.status_code == 200, f"код {response.status_code}, {response.text}"
 
-    # удаляем заказ
+    
     response = requests.delete(f'{base_url}/store/order/{order_id}')
     assert response.status_code == 200, f"заказ {order_id} не удален"
